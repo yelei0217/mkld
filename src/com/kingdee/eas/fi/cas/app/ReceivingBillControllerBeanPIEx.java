a9f31b95b28cec3bdc6c1491a17f601a7884ace1
yelei0217 <ye_lei0217@126.com>
2022-08-09 02:20:52 +0800

    init project

---
+++ src/com/kingdee/eas/fi/cas/app/ReceivingBillControllerBeanPIEx.java
@@ -0,0 +1,15 @@
+package com.kingdee.eas.fi.cas.app;
+
+public class ReceivingBillControllerBeanPIEx extends
+		ReceivingBillControllerBean {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1482146512956572715L;
+	
+	
+	//1、判断单据是否是又
+	
+
+}
